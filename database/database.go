931be145323d9e01cdb40608be69d3bb10e45fad
Marco Marassi <thtg88@users.noreply.github.com>
2020-09-20 22:12:06 +0100

    Added comments to database file

--- database/database.go
+++ database/database.go
@@ -11,10 +11,14 @@ import (
 
 var db *gorm.DB
 
+// Instance Returns the existing GORM DB instance
 func Instance() *gorm.DB {
 	return db
 }
 
+// Connect to a Postgres database.
+// The function fetches environment variables to compose the DSN string to connect
+// And assigns the DB instance to the module db var
 func Connect() *gorm.DB {
 	var err error
 
