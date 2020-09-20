81d3b6405d8f21eee7e1160cc3708197de19c0cf
Marco Marassi <thtg88@users.noreply.github.com>
2020-09-20 14:02:04 +0100

    Display 404 if user not found

--- app/controllers/userController.go
+++ app/controllers/userController.go
@@ -7,10 +7,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type NotFoundResponse struct {
+	Errors  map[string][]string
+	Message string
+}
+
 func UsersShow(c *fiber.Ctx) error {
 	var user models.User
 
-	database.Instance().First(&user, c.Params("id"))
+	result := database.Instance().First(&user, c.Params("id"))
+
+	if result.RowsAffected == 0 {
+		errors := make(map[string][]string)
+		errors["NotFound"] = []string{"User not found."}
+
+		return c.Status(fiber.StatusNotFound).JSON(&NotFoundResponse{
+			Errors:  errors,
+			Message: "There was a problem processing your request",
+		})
+	}
 
 	return c.JSON(user)
 }
