5d8739954b9e1fbc13b31e72dc35ab57669189fa
Marco Marassi <thtg88@users.noreply.github.com>
2020-09-27 18:39:04 +0100

    Moved HTTP layer definition to internal module

--- cmd/goDemoApi/goDemoApi.go
+++ cmd/goDemoApi/goDemoApi.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"goDemoApi/app/controllers"
 	"goDemoApi/database"
+	"goDemoApi/internal/goDemoApi/http"
 	"goDemoApi/queue"
 	"log"
-	"os"
 
-	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -23,20 +20,7 @@ func main() {
 
 	queue.InitializeRedis()
 
-	app := gin.Default()
-
-	authorized := app.Group("/", gin.BasicAuth(gin.Accounts{
-		os.Getenv("BASIC_AUTH_USERNAME"): os.Getenv("BASIC_AUTH_PASSWORD"),
-	}))
-
-	api := authorized.Group("/api")
-	v1 := api.Group("/v1")
-	v1.GET("/users/:id", controllers.UsersShow)
-	v1.POST("/contact-requests", controllers.ContactRequestsStore)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-	app.Run(fmt.Sprintf(":%s", port))
+	http.Init()
+	http.Routes()
+	http.Serve()
 }
---
+++ internal/goDemoApi/http/http.go
@@ -0,0 +1,43 @@
+package http
+
+import (
+	"fmt"
+	"goDemoApi/app/controllers"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var app *gin.Engine
+
+// Instance return an instance of the HTTP app
+func Instance() *gin.Engine {
+	return app
+}
+
+// Init initializes the Gin default internal app
+func Init() {
+	app = gin.Default()
+}
+
+// Routes define the routes and middlewares for the existing app
+func Routes() {
+	authorized := app.Group("/", gin.BasicAuth(gin.Accounts{
+		os.Getenv("BASIC_AUTH_USERNAME"): os.Getenv("BASIC_AUTH_PASSWORD"),
+	}))
+
+	api := authorized.Group("/api")
+	v1 := api.Group("/v1")
+	v1.GET("/users/:id", controllers.UsersShow)
+	v1.POST("/contact-requests", controllers.ContactRequestsStore)
+}
+
+// Serve serves the Gin app from the port environment variable (if provided)
+func Serve() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	app.Run(fmt.Sprintf(":%s", port))
+}
